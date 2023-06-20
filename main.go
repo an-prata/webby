34d6336ec8bcdb80ab2372561cefc7ad67ba622f
Evan Overman <evanrileyoverman@gmail.com>
2023-06-20 12:44:32 -0700

    use new configuration improvements

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ package main
 import (
 	"io"
 	"net/http"
-	"os"
 
 	"github.com/an-prata/webby/logger"
 	"github.com/an-prata/webby/server"
@@ -18,26 +17,50 @@ func defaultResponse(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	log, err := logger.NewLog(logger.All, logger.None, "")
+	log, err := logger.NewLog(logger.All, logger.All, "")
 
 	if err != nil {
 		panic(err)
 	}
 
+	defer log.Close()
 	opts, err := server.LoadConfigFromPath("/etc/webby/config.json")
 
 	if err != nil {
 		log.LogErr(err.Error())
-		log.LogWarn("Using default configuration")
+		log.LogWarn("Using default configuration due to errors")
+	}
+
+	if opts.Log != "" {
+		log.LogInfo("Opening '" + opts.Log + "' for recording logs")
+		err = log.OpenFile(opts.Log)
+
+		if err != nil {
+			log.LogErr("Could not open '" + opts.Log + "' for logging")
+		}
+	}
+
+	if opts.LogLevelPrint > logger.All || opts.LogLevelPrint < 0 {
+		log.LogErr("Unexpected log level for printing, using 'All'")
+		opts.LogLevelPrint = logger.All
+	} else {
+		log.Printing = opts.LogLevelPrint
+	}
+
+	if opts.LogLevelRecord > logger.All || opts.LogLevelRecord < 0 {
+		log.LogErr("Unexpected log level for recording, using 'All'")
+		opts.LogLevelRecord = logger.All
+	} else {
+		log.Saving = opts.LogLevelRecord
 	}
 
 	server, err := server.NewServer(opts, &log)
 
 	if err != nil {
 		log.LogErr(err.Error())
-		os.Exit(1)
+		return
 	}
 
 	log.LogErr(server.Start().Error())
-	os.Exit(1)
+	return
 }
